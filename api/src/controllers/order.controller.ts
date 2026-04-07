e3b20c0d71d41e36dc3e94593c7f84b6b5b591dd
FajarWnugra <fajarwn474@gmail.com>
2026-04-07 17:42:32 +0700

    edit controller n route

--- api/src/controllers/order.controller.ts
+++ api/src/controllers/order.controller.ts
@@ -43,3 +43,35 @@ export async function createOrderController(req: Request, res: Response) {
     redirectUrl: snapResponse.redirect_url,
   });
 }
+
+export async function midtransWebhook(req: Request, res: Response) {
+  try {
+    const notification = req.body;
+
+    const orderId = notification.order_id;
+    const transactionStatus = notification.transaction_status;
+
+    // ambil ID asli dari ORDER-1-xxxxx
+    const realOrderId = parseInt(orderId.split("-")[1]);
+
+    let status: "PENDING" | "PAID" | "FAILED" = "PENDING";
+
+    if (transactionStatus === "settlement") {
+      status = "PAID";
+    } else if (transactionStatus === "pending") {
+      status = "PENDING";
+    } else if (transactionStatus === "cancel" || transactionStatus === "deny" || transactionStatus === "expire") {
+      status = "FAILED";
+    }
+
+    await prisma.order.update({
+      where: { id: realOrderId },
+      data: { status },
+    });
+
+    res.sendStatus(200);
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
+}
--- api/src/routes/order.route.ts
+++ api/src/routes/order.route.ts
@@ -1,8 +1,9 @@
 import express from "express";
-import { createOrderController } from "../controllers/order.controller.js";
+import { createOrderController, midtransWebhook } from "../controllers/order.controller.js";
 
 const router = express.Router();
 
 router.route("/").post(createOrderController);
+router.post("/midtrans/webhook", midtransWebhook);
 
 export default router;
